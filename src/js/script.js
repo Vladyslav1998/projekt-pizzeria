a59be0092d81ac7bbd0ca195e0572fb8b6e9ee6e
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2022-04-15 01:49:45 +0200

    Fix bugs in initData and coments strings 36, 42

--- src/js/script.js
+++ src/js/script.js
@@ -33,20 +33,20 @@
     },
   };
 
-  const classNames = {
-    menuProduct: {
-      wrapperActive: 'active',
-      imageVisible: 'active',
-    },
-  };
+  // const classNames = {
+  //   menuProduct: {
+  //     wrapperActive: 'active',
+  //     imageVisible: 'active',
+  //   },
+  // };
 
-  const settings = {
-    amountWidget: {
-      defaultValue: 1,
-      defaultMin: 1,
-      defaultMax: 9,
-    }
-  };
+  // const settings = {
+  //   amountWidget: {
+  //     defaultValue: 1,
+  //     defaultMin: 1,
+  //     defaultMax: 9,
+  //   }
+  // };
 
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
@@ -82,33 +82,25 @@
   }
 
   const app = {
-    initMenu: function(){
-    const thisApp = this;
-
-    console.log('thisApp.data:', thisApp.data);
-
-    for(let productData in thisApp.data.products) {
-      new Product(productData, thisApp.data.products[productData]);
-    }
-  },
-
-  init: function(){
-    const thisApp = this;
-    console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
-    console.log('classNames:', classNames);
-    console.log('settings:', settings);
-    console.log('templates:', templates);
-
-    thisApp.initData();
-    thisApp.initMenu();
+    initMenu: function () {
+      const thisApp = this;
+      console.log('thisApp.data: ', thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
 
-    initData: function  () {
+    initData: function(){
       const thisApp = this;
+
       thisApp.data = dataSource;
-    }
+    },
   };
 
   app.init();
 }
+
+
+
+
+
