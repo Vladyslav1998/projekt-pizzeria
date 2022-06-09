fe95d23051a2f4a3da6251005bd8356a46028b3b
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2022-06-10 00:07:20 +0200

    Add Widget function

--- src/js/script.js
+++ src/js/script.js
@@ -206,9 +206,35 @@
     constructor(element) {
       const thisWidget = this;
 
+      thisWidget.getElements(element);
+
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
     }
+
+    getElements(element) {
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
+    setValue(value) {
+      const thisWidget = this;
+
+        const newValue = parseInt(value);
+
+        /* TODO: Add validation */
+        if (thisWidget.value !== newValue && !isNaN(newValue)) {
+        thisWidget.value = newValue;
+        }
+
+        thisWidget.value = newValue;
+        thisWidget.input.value = thisWidget.value;
+    }
+
   }
 
   const app = {
