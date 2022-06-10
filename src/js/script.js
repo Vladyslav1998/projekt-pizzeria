40658cfd4614ac086919236285679170f950849f
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2022-06-10 12:03:38 +0200

    Add initActions function

--- src/js/script.js
+++ src/js/script.js
@@ -207,6 +207,7 @@
       const thisWidget = this;
 
       thisWidget.getElements(element);
+      thisWidget.initActions();
 
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
@@ -219,22 +220,42 @@
       thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
       thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+      thisWidget.setValue(thisWidget.input.value);
+
     }
+
 
     setValue(value) {
       const thisWidget = this;
 
-        const newValue = parseInt(value);
+      const newValue = parseInt(value);
 
-        /* TODO: Add validation */
-        if (thisWidget.value !== newValue && !isNaN(newValue)) {
+      /* TODO: Add validation */
+      if (thisWidget.value !== newValue && !isNaN(newValue)) {
         thisWidget.value = newValue;
-        }
+      }
 
-        thisWidget.value = newValue;
-        thisWidget.input.value = thisWidget.value;
+      thisWidget.value = newValue;
+      thisWidget.input.value = thisWidget.value;
     }
 
+    initActions() {
+     const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+    }
   }
 
   const app = {
