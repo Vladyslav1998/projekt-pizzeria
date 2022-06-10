06b8138586606b623502ff5a1bbedfd1e1496e77
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2022-06-10 16:39:28 +0200

    Add code

--- src/js/script.js
+++ src/js/script.js
@@ -234,24 +234,24 @@
       if (thisWidget.value !== newValue && !isNaN(newValue)) {
         thisWidget.value = newValue;
       }
-
+      
       thisWidget.value = newValue;
       thisWidget.input.value = thisWidget.value;
     }
 
     initActions() {
-     const thisWidget = this;
+      const thisWidget = this;
 
-      thisWidget.input.addEventListener('change', function(){
+      thisWidget.input.addEventListener('change', function () {
         thisWidget.setValue(thisWidget.input.value);
       });
 
-      thisWidget.linkDecrease.addEventListener('click', function(event){
+      thisWidget.linkDecrease.addEventListener('click', function (event) {
         event.preventDefault();
         thisWidget.setValue(thisWidget.value - 1);
       });
 
-      thisWidget.linkIncrease.addEventListener('click', function(event){
+      thisWidget.linkIncrease.addEventListener('click', function (event) {
         event.preventDefault();
         thisWidget.setValue(thisWidget.value + 1);
       });
